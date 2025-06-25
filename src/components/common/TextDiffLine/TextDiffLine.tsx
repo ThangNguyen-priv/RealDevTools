84d9212780c9a846dd97e010024808f2e6b7cfcb
Nguyễn Văn Thắng <thangnguyn18@gmail.com>
2025-06-25 16:38:14 +0700

    cap nhat TextDiffLine

--- src/components/common/TextDiffLine/TextDiffLine.tsx
+++ src/components/common/TextDiffLine/TextDiffLine.tsx
@@ -1,16 +1,12 @@
 import type { TextDiffLineProps } from "../../../types/pages/textdiff.type";
 
-export default function TextDiffLine({index,isDifferent, content, variant}: TextDiffLineProps) 
-{
-    const Color = variant === "left" ? "bg-green-500 text-green-900" : "bg-red-200 text-red-900";
+export default function TextDiffLine({ index, isDifferent, content, variant }: TextDiffLineProps) {
+    const Color = variant === "left" ? "bg-green-500 text-green-900 underline" : "bg-red-200 text-red-900 underline";
     return (
         <div
             key={`left-${index}`}
-            className={`whitespace-pre-wrap font-mono flex justify-between 
-            ${isDifferent 
-            ?  (`${Color} h-8 p-2 rounded`)
-            : ""}`
-            }>
+            className={`whitespace-pre-wrap font-mono flex justify-between
+            ${isDifferent ? `${Color} h-7 p-2 rounded flex items-center hover:text-lg` : ""}`}>
             <div className="flex-grow">{content}</div>
             <div className="ml-4 text-sm flex-shrink-0">{index}</div>
         </div>
--- src/components/features/TextDiffContent/TextDiffResult.tsx
+++ src/components/features/TextDiffContent/TextDiffResult.tsx
@@ -4,16 +4,24 @@ export default function TextDiffResult({ resultLeft, resultRight, same }: TextDi
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-2">
             {same ? (
-                <div className="text-green-600 font-semibold text-xl col-span-2 flex justify-center rounded-2xl border p-3">2 input không khác nhau</div>
+                <div className="text-green-600 font-semibold text-xl col-span-2 flex justify-center rounded-2xl border p-3">
+                    2 input không khác nhau
+                </div>
             ) : resultLeft.length > 0 && resultRight.length > 0 ? (
                 <>
-                    <div className="space-y-1 border rounded-2xl p-2">
-                        <h2 className="text-lg font-semibold flex justify-center underline">Left Differences:</h2>
-                        {resultLeft}
+                    <div>
+                        <h2 className="text-lg font-semibold flex justify-center underline mb-2">
+                            Left Differences:
+                        </h2>
+                        <div className="space-y-1 border rounded-2xl p-2">
+                            {resultLeft}</div>
                     </div>
-                    <div className="space-y-1 border rounded-2xl p-2">
-                        <h2 className="text-lg font-semibold flex justify-center underline">Right Differences:</h2>
-                        {resultRight}
+                    <div>
+                        <h2 className="text-lg font-semibold flex justify-center underline mb-2">
+                            Right Differences:
+                        </h2>
+                        <div className="space-y-1 border rounded-2xl p-2">
+                            {resultRight}</div>
                     </div>
                 </>
             ) : null}
